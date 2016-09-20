a573655920fb77a819b8e0c23bca77c57a15bc95
Tom Elliott <tom.elliott@nyu.edu>
2016-09-20 15:58:14 -0500

    basic functionality

--- filter.py
+++ filter.py
@@ -1,13 +1,16 @@
 """
-Python 3 script template (changeme)
+filter seminar xml
 """
 
 import argparse
 from functools import wraps
 import inspect
 import logging
+from lxml import etree
 import os
 import re
+from slugify import slugify
+from statistics import mean, stdev
 import sys
 import traceback
 
@@ -18,8 +21,11 @@ POSITIONAL_ARGUMENTS = [
         'case-insensitive string: DEBUG, INFO, WARNING, or ERROR'],
     ['-v', '--verbose', False, 'verbose output (logging level == INFO)'],
     ['-w', '--veryverbose', False,
-        'very verbose output (logging level == DEBUG)'],
+        'very verbose output (logging level == DEBUG)']
 ]
+NS = {
+    't': 'http://www.tei-c.org/ns/1.0'
+}
 
 
 def arglogger(func):
@@ -39,8 +45,77 @@ def main(args):
     """
     main function
     """
-    # logger = logging.getLogger(sys._getframe().f_code.co_name)
-    pass
+    logger = logging.getLogger(sys._getframe().f_code.co_name)
+    schema_doc = etree.parse('tei-epidoc.rng')
+    schema = etree.RelaxNG(schema_doc)
+
+    docs = []
+    whence = os.path.realpath(args.whence)
+    for root, dirs, files in os.walk(whence):
+        for fn in files:
+            if fn[-4:] == '.xml':
+                if 'template' not in fn:
+                    fpath = os.path.join(root, fn)
+                    title = ': '.join(
+                        fpath.partition(whence)[2][1:].split('/'))
+                    try:
+                        doc = etree.parse(fpath)
+                    except etree.XMLSyntaxError as e:
+                        logger.error('{}\n{}{}'.format(title, ' '*6, e))
+                    else:
+                        docs.append({
+                            'title': title,
+                            'doc': doc
+                            })
+        if '.git' in dirs:
+            dirs.remove('.git')
+    print('\n{}'.format('-'*80))
+    print('number of well-formed files: {}'.format(len(docs)))
+
+    # test validation
+    v = 0
+    for d in docs:
+        if (schema.validate(d['doc'])):
+            v += 1
+        else:
+            logger.error('{}\n{}{}'.format(title, ' '*6, 'invalid'))
+    print('number of valid files: {}'.format(v))
+
+    # number of words in div edition
+    words_all = []
+    words_count = []
+    words_unique = []
+    words_unique_count = []
+    tags = []
+    for d in docs:
+        doc = d['doc']
+        divs = doc.xpath("//t:div[@type='edition']", namespaces=NS)
+        if len(divs) != 1:
+            logger.error(
+                '{}\n{}{}'.format(title, ' '*6, 'wrong number of divs'))
+        text = etree.tostring(divs[0], method='text', encoding='UTF-8')
+        words = text.split()
+        words_all.extend(words)
+        d['words'] = len(words)
+        words_count.append(len(words))
+        words = list(set([w.lower() for w in words]))
+        words_unique.extend(words)
+        d['words_unique'] = len(words)
+        words_unique_count.append(len(words))
+        descendants = divs[0].xpath('descendant::t:*', namespaces=NS)
+        tags.extend([e.tag for e in descendants])
+
+    print('total word count across all files: {}'.format(len(words_all)))
+    print('mean words per edition: {}'.format(mean(words_count)))
+    print('standard deviation in words per edition: {}'
+          ''.format(stdev(words_count)))
+    print('total unique words across all files: {}'.format(len(words_unique)))
+    print('mean unique words per edition: {}'.format(mean(words_unique_count)))
+    print('standard deviation in unique words per edition: {}'
+          ''.format(stdev(words_unique_count)))
+    tags = [t.split('}')[1] for t in tags]
+    tags = sorted(list(set(tags)))
+    print('tags used in editions: {}'.format(', '.join(tags)))
 
 
 if __name__ == "__main__":
@@ -68,6 +143,7 @@ if __name__ == "__main__":
                 p[0],
                 p[1],
                 **d)
+        parser.add_argument('whence', type=str, help="where to start looking")
         # example positional argument
         # parser.add_argument(
         #     'foo',
